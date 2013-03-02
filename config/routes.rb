ccb733c93d8403715686bc57b64ffb7e077e7e4a
Ignacio Huerta <ignacio@ihuerta.net>
2013-03-02 10:46:17 +0100

    Closes #13, accion que muestra el semaforo en verde/amarillo

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Signame::Application.routes.draw do
   match 'users/:id/activate_from_email/:key' => 'users#activate', :as => 'activate_from_email'
 
   match 'search' => 'front#search', :as => 'site_search'
+  
+  match 'semaforo' => 'front#semaforo'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
